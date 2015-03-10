3b4caebdda13d835aad102d349bead5ed7c7cd43
Joe Strong <joestrong@gmail.com>
2015-03-10 19:46:34 +0000

    Added TitleBar, and using urllib instead of curl Currently broken code

--- main.py
+++ main.py
@@ -2,31 +2,44 @@
 
 from gi.repository import Gtk
 from subprocess import call
+import urllib.request
 
 class BrowserWindow(Gtk.Window):
 
-    textarea = Gtk.TextView()
 
     def __init__(self):
         Gtk.Window.__init__(self, title="Ascii Browser")
-        vert = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        horiz = Gtk.Box()
-        textbox = Gtk.Entry()
-        button = Gtk.Button('Get Website')
-        button.connect('clicked', self.request)
-        horiz.add(textbox)
-        horiz.add(button)
-        vert.add(horiz)
+        self.set_default_size(500, 400)
+        vert = Gtk.VBox()
+
+        topBar = self.buildTopBar()
+        vert.add(topBar)
+
+        self.textarea = Gtk.Label()
+        self.textarea.set_text("Blank")
         vert.add(self.textarea)
+
         self.add(vert)
 
-    def request(self, widget):
-        result = self.getWebData()
-        self.textarea.text = result
+    def buildTopBar(self):
+        topBar = Gtk.HeaderBar()
+        self.addressBar = Gtk.Entry()
+        self.addressBar.set_text('http://www.google.com')
+        button = Gtk.Button('Get Website')
+        button.connect('clicked', self.request)
+        topBar.pack_start(self.addressBar)
+        topBar.pack_end(button)
+        return topBar
 
-    def getWebData(self):
-        result = call(["curl", "http://www.google.com"])
-        return result
+    def request(self, widget):
+        result = self.getWebData(self.addressBar.get_text())
+        self.textarea.set_text(result)
+
+    def getWebData(self, url):
+        result = urllib.request.urlopen(url)
+        html = result.read()
+        html = str(html)
+        return html
 
 win = BrowserWindow()
 win.connect('delete-event', Gtk.main_quit)
