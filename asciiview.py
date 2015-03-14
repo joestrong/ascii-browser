c1b98c4e48bbfb39ce04e78a7355907253d6b1e8
Joe Strong <joestrong@gmail.com>
2015-03-14 14:59:53 +0000

    Got screenshot to trigger after webview has loaded

--- asciiview.py
+++ asciiview.py
@@ -5,16 +5,22 @@ from gi.repository import Gtk, Gdk, WebKit
 class AsciiView(Gtk.VBox):
     def __init__(self):
         Gtk.VBox.__init__(self)
+
         self.webview = WebKit.WebView()
-        self.imageview = Gtk.Image()
+        self.webview.connect("notify::load-status", self.render_image)
         self.add(self.webview)
+
+        self.imageview = Gtk.Image()
         self.add(self.imageview)
 
     def get_url(self, uri):
         self.webview.load_uri(uri)
 
-        width = self.webview.get_allocation().width
-        height = self.webview.get_allocation().height
-        gdkwin = self.webview.get_window()
-        pixbuf = Gdk.pixbuf_get_from_window(gdkwin, 0, 0, width, height)
-        self.imageview.set_from_pixbuf(pixbuf)
+    def render_image(self, web_view, property_type):
+        if(web_view.get_load_status() == WebKit.LoadStatus.FINISHED):
+            width = self.webview.get_allocation().width
+            height = self.webview.get_allocation().height
+            gdkwin = self.webview.get_window()
+
+            pixbuf = Gdk.pixbuf_get_from_window(gdkwin, 0, 0, width, height)
+            self.imageview.set_from_pixbuf(pixbuf)
